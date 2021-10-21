420b26e67e94931f6a722ca9be6a9f67bc83feab
Kaushal112411 <Kaushal112411@gmai.com>
2021-10-21 15:00:23 +0530

    first commit

--- src/components/Add.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Add({handleAdd, newTask, resetState}) {
-    console.log(newTask);
-    return (
-        <button id="btn" onClick={()=>{handleAdd(newTask, resetState)}}>ADD</button>
-    )
-}
--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,24 @@
-import React,{useState, useEffect} from "react";
-import "./../styles/App.css";
-import Taskarea from "./Textarea" 
-import Add from "./Add"
-import Task from "./Task"
+import React from 'react';
+import { useEffect } from 'react';
+import {useState} from 'react';
 
-function App() 
-{    
-    const [tasks , setTasks] = useState([]);
-
-	 function handleAdd(newTask, resetState) {
-		 
-		if(newTask.trim().length) {
-        setTasks( prevTasks => {return [...prevTasks, newTask]});
-		resetState();
-	  }
-	}
-
-	 function onDelete(id) {
-		
-		 setTasks((prevTasks) => {
-             return prevTasks.filter((task, index) => {
-			 return index != id;
-		 });
-		 });
-	 }
-
-	 function updateEdit(id, value) {
-		
-		if(value.trim().length) {
-		 let newtasks = tasks.slice(0, id);
-		 newtasks.push(value);
-		 tasks.slice(id+1).forEach(item => newtasks.push(item));
-		 
-		 setTasks(newtasks);
-		}
-	 }
-
-										 
-	return (
-	<div id="main">
-	   <div className="container">
-           <h2 id="heading" > todo list</h2>
-		   {   
-			   tasks.map((task, index) => {
-                  return <Task key={index} id={index}  data={task} onDelete={onDelete} updateEdit={updateEdit} />
-		       })
-		   }
-          
-		  <Taskarea handleAdd={handleAdd} />
-		  
-	   </div>
-	   
-	</div>
-	);
-}
-
+const App=()=> {
+    const [prod,setPro]=useState({})
+    
+    useEffect(()=>{
+        fetch("https://fakestoreapi.com/products")
+        .then(res => res.json())
+        .then((res) => {
+            setPro(res)
+            })
+    },[])
+    let arr=JSON.stringify(prod)
+    return (
+        <>
+            <p>hjwfiohj</p>
+            {arr}
+        </>
+    );
+};
 
 export default App;
